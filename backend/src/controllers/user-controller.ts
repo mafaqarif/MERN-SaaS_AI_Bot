e10b098e200071eec2ff20932e575f2ae932021c
AfaqArif12345 <muhammad.afaqarif97@gmail.com>
2024-11-18 00:09:47 -0500

    user signup added

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User";
+import { hash } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -13,4 +14,22 @@ export const getAllUsers = async (
   } catch (error) {
     return res.status(500).json({ message: "Error", cause: error });
   }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log("body " + req.body);
+  const { name, email, password } = await req.body;
+  // if (!password) {
+  //   throw new Error("Password is required for hashing");
+  // }
+  const hashedPassword = await hash(password || "", 10);
+  const user = new User({ name, email, hashedPassword });
+  await user.save();
+  return res
+    .status(201)
+    .json({ message: "User created successfully", id: user._id.toString() });
 };
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controller";
+import { getAllUsers, userSignup } from "../controllers/user-controller";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
+userRouter.post("/signup", userSignup);
 
 export default userRouter;
